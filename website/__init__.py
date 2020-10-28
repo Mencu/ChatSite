02c53df13f0189854988dcd4f28c7055d94a6597
Andrei <mancu.andrei@gmail.com>
2020-10-28 18:22:32 +0200

    Managed and added additional files for sever deployment and sql database.

---
+++ website/__init__.py
@@ -0,0 +1,39 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'tables.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError as e:
+        print("[EXCEPTION OSERROR_mkdir_instancePath]", e)
+
+    # a simple page that says hello
+    @app.route('/')
+    def hello():
+        return 'Hello, World!'
+
+    from . import db
+    db.init_app(app)
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app('test', '')
+    app.run(host='0.0.0.0', port=5000, debug=True)
